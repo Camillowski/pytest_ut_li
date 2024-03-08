263a7e0371be46ca0dcb2b10138fb37ad84d7612
Kamil Piasecki <vhnb63@motorolasolutions.com>
2024-03-08 17:50:11 +0100

    Fixtures

--- tests/chp3/video1/test_fixtures_start.py
+++ tests/chp3/video1/test_fixtures_start.py
@@ -2,15 +2,17 @@ import pytest
 from scripts import data_processor, map_population_update
 
 
-def test_data_population_update():
-    """
-    Happy Path test
-    """
-    map_data_location = 'tests/resources/cities/clean_map.csv'
-    data = data_processor.csv_reader(map_data_location)
+@pytest.fixture(scope = "session")
+def data_file():
+    yield "tests/resources/cities/clean_map.csv"
+
+
+@pytest.fixture(scope = "function")
+def prep_data(data_file):
+    data = data_processor.csv_reader(data_file)
     data_to_transform = map_population_update.MapData(data, False)
 
-    population_dict = {
+    pop_dict = {
             'Andorra': 77142,
             'Argentina': 44780677,
             'Cape Verde': 546388,
@@ -23,38 +25,31 @@ def test_data_population_update():
             'United States': 329064917
     }
 
-    data_to_transform.add_population(population_dict)   # transform object in place
+    assert data_to_transform._updated is False
+    data_to_transform.add_population(pop_dict)   # transform object in place
+
+    yield data_to_transform, pop_dict
+
+
+def test_data_population_update(prep_data):
+    """
+    Happy Path test
+    """
+    data_to_transform, _ = prep_data
 
     for row in data_to_transform.get_data():
         assert 'Population' in row
         assert 'Updated' in row
 
 
-def test_data_population_no_update():
+def test_data_population_no_update(prep_data):
     """
     Sad Path test: We don't want the data transformed twice
 
     """
-
-    map_data_location = 'tests/resources/cities/clean_map.csv'
-    data = data_processor.csv_reader(map_data_location)
-    data_to_transform = map_population_update.MapData(data, False)
-
-    population_dict = {
-            'Andorra': 77142,
-            'Argentina': 44780677,
-            'Cape Verde': 546388,
-            'Germany': 83670653,
-            'Greece': 10473455,
-            'India': 1366417754,
-            'Japan': 128860301,
-            'Morocco': 36471769,
-            'Senegal': 16296364,
-            'United States': 329064917
-    }
-
-    data_to_transform.add_population(population_dict)   # transform object in place
+    transformed_data, population_dict = prep_data
+    
     # Try calling the function again
     with pytest.raises(Exception) as e:
-        data_to_transform.add_population(population_dict)
+        transformed_data.add_population(population_dict)
     assert str(e.value) == 'You cannot transform the data twice'
